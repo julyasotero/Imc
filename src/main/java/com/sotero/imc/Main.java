dd9ea9261b45c3e4d8b48c0048c0c7434741fdcd
André Sotero <andre.sotero@telefonica.com>
2021-05-12 20:22:59 -0300

    criação da casca

---
+++ src/main/java/com/sotero/imc/Main.java
@@ -0,0 +1,18 @@
+package com.sotero.imc;
+
+import java.util.Scanner;
+
+public class Main {
+    
+    private static final Scanner scanf = new Scanner(System.in);
+    
+    public static void main(String[] args) {
+        System.out.println(":::...IMC...:::");
+        
+        
+        
+        
+        System.out.println(":::...FIM IMC...:::");
+    }
+    
+}
